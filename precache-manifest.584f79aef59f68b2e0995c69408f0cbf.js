a782a1fd0d47eacd280781150d9ee7aac256545b
Tom Barnes <barnes.thomas.c@gmail.com>
2018-12-18 15:40:59 -0500

    Updates

---
+++ precache-manifest.584f79aef59f68b2e0995c69408f0cbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d901d19419b120b6e65",
+    "url": "/meme-generator-react/static/js/runtime~main.7d901d19.js"
+  },
+  {
+    "revision": "9e7993a7dd724fd531a9",
+    "url": "/meme-generator-react/static/js/main.9e7993a7.chunk.js"
+  },
+  {
+    "revision": "f25b8d03010eaca55f04",
+    "url": "/meme-generator-react/static/js/1.f25b8d03.chunk.js"
+  },
+  {
+    "revision": "9e7993a7dd724fd531a9",
+    "url": "/meme-generator-react/static/css/main.1cca01b5.chunk.css"
+  },
+  {
+    "revision": "5c8987260f0d8bba908657c7f1d1be48",
+    "url": "/meme-generator-react/index.html"
+  }
+];
